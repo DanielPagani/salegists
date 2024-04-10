606e998b607e91ea5472e56421ff7299c3523dff
Daniel Pagani <danielpagani.dev@gmail.com>
2024-04-10 12:54:46 -0300

    refactor: Home.vue to use MainContent component for header and content sections

---
+++ components/Logo/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center gap-1 select-none">
+    <span class="text-2xl">😃</span>
+
+    <span class="font[Inter] font-bold text-gray-700 text-lg md:text-xl">SaleGists</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- modules/landing-page/screens/Home/Home.vue
+++ modules/landing-page/screens/Home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <Header/>
-  <Hero/>
+  <MainContent>
+    <template #header>
+      <Header @wants-be-creator="handleLogin" />
+    </template>
+
+    <template #content>
+      <Hero @wants-be-creator="handleLogin" />
+    </template>
+  </MainContent>
 </template>
 
 <script setup lang="ts">
-import Header from "@/modules/landing-page/screens/Home/components/Header/Header.vue"
-import Hero from "@/modules/landing-page/screens/Home/components/Hero/Hero.vue";
+import Header from '@/modules/landing-page/components/Header/Header.vue'
+import Hero from '@/modules/landing-page/components/Hero/Hero.vue'
+
+const router = useRouter()
+
+const handleLogin = () => {
+  router.push('/auth/login')
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
